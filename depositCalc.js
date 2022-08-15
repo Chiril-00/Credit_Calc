667e0f4aa7d59e02a7ca0fcd290508d5134dd3b9
Chiril <kskiril@gmail.com>
2022-08-15 14:28:49 +0300

    nothing special

--- depositCalc.js
+++ depositCalc.js
@@ -1,8 +1,13 @@
 document.querySelector('.calcDeposit').addEventListener('click', function() {
-    const depositAmount = document.querySelector('.depositAmount').value;
-    const depositTime = document.querySelector('.depositTime').value;
+    let depositAmount = document.querySelector('.depositAmount').value;
+    let depositTime = document.querySelector('.depositTime').value;
 
     let firstYearProfit = (depositAmount * 1.01);
-    
+    for (i=1; i <= depositTime; i++) {
+        depositAmount *= 1.01;
+    }
+
     document.querySelector('.profit').textContent = (`With 1% yearly profit, next year you will have ${firstYearProfit} on your bank account.`);
+    document.querySelector('.finalProfit').textContent = (`With 1% yearly profit, by the end of ${depositTime} years period, you will have ${depositAmount} on your bank account.`);
+
 })
